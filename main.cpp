5567014fd507ea80a729bb6d94e50e3e466aaa47
Павел Кравцов <Ktylha@vk.com>
2024-06-06 11:25:48 +0300

    Proverka

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+
+void print_vec(const std::vector <int>& vec){
+    for (auto& v : vec){
+        std::cout << v << " ";
+    }
+    std::cout << std::endl;
+}
 
 int main() {
-    std::cout << "Hello, Git!" << std::endl;
+    int N, l; 
+    std::cin >> N;
+    std::vector<int> vec;
+    while (N--){
+        std::cin >> l;
+        vec.push_back(l);
     }
+    std::sort(vec.begin(),vec.end());
+    print_vec(vec);
+    std::cout<< "БИНГО" << std::endl;
+  // put your code here
+  return 0;
+}
